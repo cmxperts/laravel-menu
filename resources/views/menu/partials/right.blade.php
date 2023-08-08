321bc1c33ef75305c59dfe3315f3ccde5f74791b
cmxperts <cmxperts@gmail.com>
2023-08-08 13:50:58 +0530

    Update

--- resources/views/menu/partials/right.blade.php
+++ resources/views/menu/partials/right.blade.php
@@ -6,17 +6,20 @@
                     <div class="form-group">
                         <label for="email" class="mr-sm-2">Name: </label>
                         <input name="menu-name" id="menu-name" type="text"
-                        class="form-control menu-name regular-text menu-item-textbox"
-                        title="Enter menu name" value="@if(isset($indmenu)){{$indmenu->name}}@endif">
+                               class="form-control menu-name regular-text menu-item-textbox"
+                               title="Enter menu name" value="@if(isset($indmenu)){{$indmenu->name}}@endif">
                         @if(request()->has('action'))
                             <button type="button" onclick="createNewMenu()" name="save_menu"
-                                class="btn btn-primary menu-save ml-2">Create Menu</button>
+                                    class="btn btn-primary menu-save ml-2">Create Menu
+                            </button>
                         @elseif(request()->has('menu'))
                             <button type="button" onclick="actualizarMenu(false)" name="save_menu"
-                                class="btn btn-primary menu-save ml-2">Save Menu</button>
+                                    class="btn btn-primary menu-save ml-2">Save Menu
+                            </button>
                         @else
                             <button type="button" onclick="createNewMenu()" name="save_menu"
-                                class="btn btn-primary menu-save ml-2">Create Menu</button>
+                                    class="btn btn-primary menu-save ml-2">Create Menu
+                            </button>
                         @endif
                     </div>
                 </form>
@@ -24,54 +27,56 @@
             </div>
             <div class="col-md-12">
                 @if(request()->get('menu') != 0 && isset($menus) && count($menus) > 0)
-                <div class="jumbotron jumbotron-fluid p-2">
-                    <div class="container">
-                        <h3>Menu Structure</h3>
-                        <div class="alert alert-info">
-                        <p class="">Place each item in the order you prefer. Click <i class="la la-angle-down" aria-hidden="true"></i> to the right of the item to display more configuration options.</p>
+                    <div class="jumbotron jumbotron-fluid p-2">
+                        <div class="container">
+                            <h3>Menu Structure</h3>
+                            <div class="alert alert-info">
+                                <p class="">Place each item in the order you prefer. Click <i class="la la-angle-down"
+                                                                                              aria-hidden="true"></i> to
+                                    the right of the item to display more configuration options.</p>
+                            </div>
                         </div>
                     </div>
-                </div>
                 @elseif(request()->get('menu') == 0)
-                <div class="jumbotron jumbotron-fluid p-2">
-                    <div class="container">
-                        <h3>Menu Creation</h3>
-                        <p class="lead">Please enter the name and select "Create menu" button</p>
+                    <div class="jumbotron jumbotron-fluid p-2">
+                        <div class="container">
+                            <h3>Menu Creation</h3>
+                            <p class="lead">Please enter the name and select "Create menu" button</p>
+                        </div>
                     </div>
-                </div>
                 @else
-                <div class="jumbotron jumbotron-fluid p-2">
-                    <div class="container">
-                        <h3>Create Menu Item</h3>
-                        <p class="lead"></p>
+                    <div class="jumbotron jumbotron-fluid p-2">
+                        <div class="container">
+                            <h3>Create Menu Item</h3>
+                            <p class="lead"></p>
+                        </div>
                     </div>
-                </div>
                 @endif
 
                 <div id="accordion" class="">
                     @if(isset($menus) && count($menus) > 0)
-                    <div class="dd nestable-menu" id="nestable">
-                        <ol class="dd-list">
-                            @foreach($menus as $key => $m)
-                                @include('cmxperts::menu.partials.loop-item', ['key' => $key])
-                            @endforeach
-                        </ol>
-                    </div>
+                        <div class="dd nestable-menu" id="nestable">
+                            <ol class="dd-list">
+                                @foreach($menus as $key => $m)
+                                    @include('cmxperts::menu.partials.loop-item', ['key' => $key])
+                                @endforeach
+                            </ol>
+                        </div>
                     @endif
                 </div>
             </div>
         </div>
     </div>
     @if(request()->get('menu') != 0)
-    <div class="card-footer">
-        <button type="button" class="btn btn-danger btn-sm submitdelete deletion menu-delete"
-            onclick="deleteMenu()" href="javascript:void(9)">Delete Menu
-        </button>
-        @if(isset($menus) && count($menus) > 0)
-        <button type="button" class="btn btn-info btn-sm"
-            onclick="updateItem()" href="javascript:void(9)">Update All Item
-        </button>
-        @endif
-    </div>
+        <div class="card-footer">
+            <button type="button" class="btn btn-danger btn-sm submitdelete deletion menu-delete"
+                    onclick="deleteMenu()" href="javascript:void(9)">Delete Menu
+            </button>
+            @if(isset($menus) && count($menus) > 0)
+                <button type="button" class="btn btn-info btn-sm"
+                        onclick="updateItem()" href="javascript:void(9)">Update All Item
+                </button>
+            @endif
+        </div>
     @endif
 </div>
